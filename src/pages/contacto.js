1cda66af7f97c22bd5fef74282c27abf4ef7303e
Victor Vazquez <victor.vamv17@gmail.com>
2023-06-20 17:13:08 -0600

    add:formulario y validaciones, boton

--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -14,10 +14,15 @@ export default function Home() {
         <h3 className={`${styles.centrar__texto} ${styles.h3} ${styles.contacto__color}`}>Contacto</h3> 
     
 
-    <form className={`${styles.formulario}  formulario1`}>
-      <div className={`${styles.campo} input-group mb-4 input-group-lg`} >
-        <span className='input-group-text'>Nombre</span>
-        <input type='text' className='form-control' placeholder='Nombre Completo'></input>
+    <form className={`${styles.formulario}  formulario1 needs-validation was-validated`} novalidate>
+      <div className={`${styles.campo} input-group mb-4 input-group-lg`} for="validationTooltipUsername">
+        <span className='input-group-text'  id="validationTooltipUsernamePrepend">Nombre</span>
+        
+        <input  type='text' className='form-control has-validation' id='validationTooltipUsername' aria-describedby="validationTooltipUsernamePrepend" placeholder='Nombre Completo' required></input>
+        <div className='invalid-tooltip'>
+          {"Es correcto"}
+        </div>
+        
       </div>
 
       <div className={`${styles.campo} input-group mb-4 input-group-lg`} >
@@ -30,15 +35,12 @@ export default function Home() {
         <label htmlFor="floatingTextarea">Comentarios</label> 
       </div>
       
-      <div className={`${styles.boton__elementos} `}>
-        <button type="input" className="btn btn-primary btn-lg">
-         
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-down-right-square fs-2" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 3.146a.5.5 0 1 0-.708.708L9.243 9.95H6.475a.5.5 0 1 0 0 1h3.975a.5.5 0 0 0 .5-.5V6.475a.5.5 0 1 0-1 0v2.768L5.854 5.146z"></path>
+      <div className={`${styles.boton__elementos} d-grid d-md-block d-md-flex justify-content-md-end`}>
+        <button type="submit" className="btn btn-success btn-lg">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-arrow-down-right-square " viewBox="0 0 16 16">
+                <path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5.854 3.146a.5.5 0 1 0-.708.708L9.243 9.95H6.475a.5.5 0 1 0 0 1h3.975a.5.5 0 0 0 .5-.5V6.475a.5.5 0 1 0-1 0v2.768L5.854 5.146z"></path>
                 </svg>
-                
                   {" Enviar "}  
-        
         </button>
       </div>
     </form>
